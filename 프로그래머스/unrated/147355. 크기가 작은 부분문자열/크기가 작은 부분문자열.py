95d978054b4b44a111d308f0d600004118666789
장민정 <131653557+JangMinJung@users.noreply.github.com>
2023-06-08 11:21:57 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.04 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -0,0 +1,10 @@
+def solution(t, p):
+    answer = 0
+    for i in range(len(t)):
+        if int(t[i:i+len(p)])<=int(p):
+            if len(t[i:i+len(p)])<len(p):
+                     break
+            else:
+                answer+=1
+        
+    return answer
